cec0f8c37999b189a64f4153016f0ede8f83eeb4
Tigran Yeranyan <tigran.e@purlin.com>
2024-03-25 12:05:42 +0400

    initial commit

---
+++ src/api/invitation-page/routes/invitation-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * invitation-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::invitation-page.invitation-page');
---
+++ src/api/invitation-page/services/invitation-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * invitation-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::invitation-page.invitation-page');
